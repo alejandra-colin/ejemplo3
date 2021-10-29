fe2efcb9487cd28c30659a97b474ca71ed3de59c
aleja <erialecolin1109@gamil.com>
2021-10-29 16:14:38 -0500

    2

--- final_velocity.py
+++ final_velocity.py
@@ -1,5 +1,6 @@
 """
 
+
 Program to compute the final velocity of an object, given initial velocity,
 acceleration and distance traveled
 
@@ -24,7 +25,7 @@ import math
 
 # Get the inputs
 v_0 = float(input("Ingrese la velocidad inicial: "))
-a = float(input("Enter acceleration: "))
+a = float(input("Ingrese aceleracion: "))
 x_0 = float(input("Enter initial position: "))
 x = float(input("Enter final position: "))
 
